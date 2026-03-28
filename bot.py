34c90609678b43fa36ea3f87de63635c868a6d51
paraturky-ops <paraturky@gmail.com>
2026-03-28 12:27:15 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,4 +98,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
